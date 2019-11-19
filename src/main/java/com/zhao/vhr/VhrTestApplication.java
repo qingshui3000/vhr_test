a89388d802267d15b2f41f9cb8e7b01a64ccd10b
qingshui3000 <zhao6582@qq.com>
2019-11-19 19:51:56 +0800

    day1

---
+++ src/main/java/com/zhao/vhr/VhrTestApplication.java
@@ -0,0 +1,13 @@
+package com.zhao.vhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhrTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhrTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhao/vhr/VhrTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhao.vhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhrTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
